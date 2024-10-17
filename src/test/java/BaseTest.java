ce1c289c48924760579ed66b30e6645aa040aa81
Nikolay Shivachev <shivachev.nikolay@gmail.com>
2024-10-17 10:49:10 +0300

    Comment lines

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,71 +1,71 @@
-import listeners.BrowserLaunchTestBehaviorObserver;
-import listeners.ExecutionSubject;
-import listeners.TestExecutionSubject;
-import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
-import webDriver.Driver;
-import webDriver.LoggingSingletonDriver;
-
-public class BaseTest {
-
-    private static final ThreadLocal<TestExecutionSubject> executionSubject = ThreadLocal.withInitial(ExecutionSubject::new);
-    private static final ThreadLocal<Driver> driver = ThreadLocal.withInitial(LoggingSingletonDriver::getInstance);
-    private ITestResult result;
-
-    static {
-        executionSubject.set(new ExecutionSubject());
-        driver.set(LoggingSingletonDriver.getInstance());
-        new BrowserLaunchTestBehaviorObserver(executionSubject.get(), driver.get());
-    }
-
-    public String getTestName() {
-        return getTestResult().getTestName();
-    }
-
-    public void setTestResult(ITestResult result) {
-        this.result = result;
-    }
-
-    public ITestResult getTestResult() {
-        return result;
-    }
-
-    public Driver getDriver() {
-        return driver.get();
-    }
-
-    @AfterSuite
-    public void afterSuite() {
-        if (driver.get() != null) {
-            driver.get().quit();
-        }
-    }
-
-    @BeforeMethod
-    public void beforeMethod(ITestResult result) throws NoSuchMethodException {
-        setTestResult(result);
-        var testClass = this.getClass();
-        var methodInfo = testClass.getMethod(getTestResult().getMethod().getMethodName());
-        executionSubject.get().preTestInit(getTestResult(), methodInfo);
-        testInit();
-        executionSubject.get().postTestInit(getTestResult(), methodInfo);
-    }
-
-    @AfterMethod
-    public void afterMethod(ITestResult result) throws NoSuchMethodException {
-        setTestResult(result);
-        var testClass = this.getClass();
-        var memberInfo = testClass.getMethod(getTestResult().getMethod().getMethodName());
-        executionSubject.get().preTestCleanup(getTestResult(), memberInfo);
-        testCleanup();
-        executionSubject.get().postTestCleanup(getTestResult(), memberInfo);
-    }
-
-    protected void testInit() {
-    }
-
-    protected void testCleanup() {
-    }
-}
+//import listeners.BrowserLaunchTestBehaviorObserver;
+//import listeners.ExecutionSubject;
+//import listeners.TestExecutionSubject;
+//import org.testng.ITestResult;
+//import org.testng.annotations.AfterMethod;
+//import org.testng.annotations.AfterSuite;
+//import org.testng.annotations.BeforeMethod;
+//import webDriver.Driver;
+//import webDriver.LoggingSingletonDriver;
+//
+//public class BaseTest {
+//
+//    private static final ThreadLocal<TestExecutionSubject> executionSubject = ThreadLocal.withInitial(ExecutionSubject::new);
+//    private static final ThreadLocal<Driver> driver = ThreadLocal.withInitial(LoggingSingletonDriver::getInstance);
+//    private ITestResult result;
+//
+//    static {
+//        executionSubject.set(new ExecutionSubject());
+//        driver.set(LoggingSingletonDriver.getInstance());
+//        new BrowserLaunchTestBehaviorObserver(executionSubject.get(), driver.get());
+//    }
+//
+//    public String getTestName() {
+//        return getTestResult().getTestName();
+//    }
+//
+//    public void setTestResult(ITestResult result) {
+//        this.result = result;
+//    }
+//
+//    public ITestResult getTestResult() {
+//        return result;
+//    }
+//
+//    public Driver getDriver() {
+//        return driver.get();
+//    }
+//
+//    @AfterSuite
+//    public void afterSuite() {
+//        if (driver.get() != null) {
+//            driver.get().quit();
+//        }
+//    }
+//
+//    @BeforeMethod
+//    public void beforeMethod(ITestResult result) throws NoSuchMethodException {
+//        setTestResult(result);
+//        var testClass = this.getClass();
+//        var methodInfo = testClass.getMethod(getTestResult().getMethod().getMethodName());
+//        executionSubject.get().preTestInit(getTestResult(), methodInfo);
+//        testInit();
+//        executionSubject.get().postTestInit(getTestResult(), methodInfo);
+//    }
+//
+//    @AfterMethod
+//    public void afterMethod(ITestResult result) throws NoSuchMethodException {
+//        setTestResult(result);
+//        var testClass = this.getClass();
+//        var memberInfo = testClass.getMethod(getTestResult().getMethod().getMethodName());
+//        executionSubject.get().preTestCleanup(getTestResult(), memberInfo);
+//        testCleanup();
+//        executionSubject.get().postTestCleanup(getTestResult(), memberInfo);
+//    }
+//
+//    protected void testInit() {
+//    }
+//
+//    protected void testCleanup() {
+//    }
+//}
