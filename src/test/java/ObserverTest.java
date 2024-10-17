221236abef6feeae256026710b163fdd6dd7c4f2
Nikolay Shivachev <shivachev.nikolay@gmail.com>
2024-10-17 13:57:18 +0300

    Uncommented tests

--- src/test/java/ObserverTest.java
+++ src/test/java/ObserverTest.java
@@ -1,31 +1,33 @@
-//import listeners.BrowserBehavior;
-//import listeners.ExecutionBrowser;
-//import org.testng.annotations.Test;
-//import pages.mainPage.MainPage;
-//import webDriver.*;
-//
-//@ExecutionBrowser(browser = Browser.CHROME, browserBehavior = BrowserBehavior.RESTART_ON_FAIL)
-//public class ObserverTest extends BaseTest {
-//
-//    private static MainPage mainPage;
-//    private static BrowserService browserService;
-//
-//    @Override
-//    public void testInit() {
-//        browserService = LoggingSingletonDriver.getInstance();
-//        mainPage = MainPage.getInstance();
+import listeners.BrowserBehavior;
+import listeners.ExecutionBrowser;
+import org.testng.annotations.Test;
+import pages.mainPage.MainPage;
+import webDriver.Browser;
+import webDriver.BrowserService;
+import webDriver.LoggingSingletonDriver;
+
+@ExecutionBrowser(browser = Browser.CHROME, browserBehavior = BrowserBehavior.RESTART_ON_FAIL)
+public class ObserverTest extends BaseTest {
+
+    private static MainPage mainPage;
+    private static BrowserService browserService;
+
+    @Override
+    public void testInit() {
+        browserService = LoggingSingletonDriver.getInstance();
+        mainPage = MainPage.getInstance();
+    }
+
+    @Test(priority = 1)
+    public void test1() {
+        mainPage.open();
+        browserService.waitForAjax();
+        browserService.waitUntilPageLoadsCompletely();
+        mainPage.assertions().assertUserNameField();
+    }
+
+//    @Test(priority = 2)
+//    public void test2() {
+//        getDriver().goToUrl("https://abv.bg");
 //    }
-//
-////    @Test(priority = 1)
-////    public void test1() {
-////        mainPage.open();
-////        browserService.waitForAjax();
-////        browserService.waitUntilPageLoadsCompletely();
-////        mainPage.assertions().assertUserNameField();
-////    }
-//
-////    @Test(priority = 2)
-////    public void test2() {
-////        getDriver().goToUrl("https://abv.bg");
-////    }
-//}
+}
